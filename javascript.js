c04f3b33b93ca0fe331f497fdf6bc235f15f5214
Adam <adam.wren@ten10.com>
2024-06-27 15:20:42 +0100

    Added dynamic board size

--- javascript.js
+++ javascript.js
@@ -1,26 +1,34 @@
+
 const board = document.getElementById("board")
 const container = document.getElementById("container")
+const button = document.createElement("button")
+
+button.textContent = "Change board size"
+container.appendChild(button)
+button.onclick = bttnInput
 
 
-function createBoard(){
-
-    const button = document.createElement("button")
-
-    button.textContent = "Change board size"
-
-    container.appendChild(button)
-
-    button.onclick = bttnInput
-
-    function bttnInput(){
-        let userInput = prompt("hello")
-        return userInput
+function bttnInput(){
+    let userInput = prompt("Please choose a new grid size (0-100)")
+    if ((userInput > 100)||
+        (userInput < 1))
+        {
+        alert("Grid size must be between 1-100")
     }
+    else {
+        board.innerHTML=""
+        createBoard(userInput)
+    }
+}
 
-    for (i = 0; i < 256; i++){
+
+function createBoard(gridSize){
+    for (i = 0; i < gridSize * gridSize; i++){
         const grid = document.createElement("div")
         grid.classList.add("gridSquares")
         board.appendChild(grid)
+        grid.style.height = 500 / gridSize + "px";
+        grid.style.width = 500 / gridSize + "px";
         grid.addEventListener("mouseover", () =>{
             grid.style.background = "red"
         });
@@ -28,4 +36,6 @@ function createBoard(){
 }
 
 
-createBoard()
+createBoard(16)
+
+
