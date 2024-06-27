39801a4330782cb1884ee8d5aa3a5ae85952c90b
Adam <adam.wren@ten10.com>
2024-06-27 12:51:22 +0100

    Added hover + colour function - trying to create responsive board size

--- javascript.js
+++ javascript.js
@@ -1,9 +1,31 @@
-const container = document.querySelector("#container")
+const board = document.getElementById("board")
+const container = document.getElementById("container")
 
-for (i = 0; i < 256; i++){
-    const i = document.createElement("div")
-    i.classList.add("grid")
-    container.appendChild(i)
+
+function createBoard(){
+
+    const button = document.createElement("button")
+
+    button.textContent = "Change board size"
+
+    container.appendChild(button)
+
+    button.onclick = bttnInput
+
+    function bttnInput(){
+        let userInput = prompt("hello")
+        return userInput
+    }
+
+    for (i = 0; i < 256; i++){
+        const grid = document.createElement("div")
+        grid.classList.add("gridSquares")
+        board.appendChild(grid)
+        grid.addEventListener("mouseover", () =>{
+            grid.style.background = "red"
+        });
+    }   
 }
 
 
+createBoard()
